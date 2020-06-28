28490ca4e9e8f81a1d5bf8215b2f87f33d59767b
Darren <DarrenDocherty@Live.com>
2020-06-28 10:08:05 +0100

    Support for banning users via IP Address
    Added support for banning users via their IP Address in addition to their Steam id

--- server.lua
+++ server.lua
@@ -15,12 +15,16 @@ AddEventHandler("playerConnecting", function(name, reject, deferrals)
 	local bans = GetBans()
 	local _source = source
 	local identifier = GetSteamIdentifier(_source)
-	
-	for k, v in pairs(bans) do 
-		if k == identifier then 
-			deferrals.defer()
-			reject("You are banned from this server. Visit Discord.gg/m9Ngbmd for help.")
-			CancelEvent()
+	local networkid = GetNetworkIdentifier(_source)
+
+	for user, ban in pairs(bans) do 
+		for k, v in pairs(ban) do
+			if v == identifier or v == networkid then
+				deferrals.defer()	
+				reject("You are banned from this server. Visit Discord.gg/m9Ngbmd for help.")
+				print("^8A banned user (" .. user .. ") attempted to connect from " .. networkid .. " using " .. identifier .. ".^7")
+				CancelEvent()
+			end
 		end
 	end
 end)
@@ -35,10 +39,9 @@ AddEventHandler("Bring", function(target, x, y, z)
 	TriggerClientEvent("Bring", target, x, y, z)
 end)
 
-
 RegisterServerEvent("Message")
 AddEventHandler("Message", function(target, message)
-	TriggerClientEvent("redem_roleplay:NotifyLeft", target, "New Message", message, "menu_textures", "menu_icon_alert", 8000)
+	TriggerClientEvent("redem_roleplay:NotifyLeft", target, "Private Message", message, "menu_textures", "menu_icon_alert", 8000)
 end)
 
 RegisterServerEvent("KickPlayer")
@@ -48,9 +51,10 @@ AddEventHandler("KickPlayer", function(target)
 end)
 
 RegisterServerEvent("BanPlayer")
-AddEventHandler("BanPlayer", function(target, banner)
+AddEventHandler("BanPlayer", function(target, username, banner)
 	local identifier = GetSteamIdentifier(target)
-	Ban(target, tostring(identifier), banner)
+	local networkid = GetNetworkIdentifier(target)
+	Ban(target, tostring(username), tostring(identifier), tostring(networkid), banner)
 end)
 
 function GetBans()
@@ -67,11 +71,16 @@ function GetBans()
 	return bans
 end
 
-function Ban(target, identifier, banner)
+function Ban(target, username, identifier, networkid, banner)
 	local bans = GetBans()
 	
-	bans[identifier] = {
+	print(target, username, identifier, networkid, banner)
+	
+	bans[username] = {
+		steamid = identifier,
+		ip = networkid,
 		banner = banner,
+		reason = "No reason specified.",
 	}
 	
 	SaveResourceFile(GetCurrentResourceName(), "bans.json", json.encode(bans), -1)
@@ -88,3 +97,21 @@ function GetSteamIdentifier(source)
 
 	return identifier
 end
+
+function GetNetworkIdentifier(source)
+	local identifier = GetPlayerIdentifiers(source)[2] or false
+
+	if (identifier == false or identifier:sub(1,2) ~= "ip") then
+		return false
+	end
+
+	return identifier
+end
+
+--[[ Auto ban if new ip or steamid is detected (for future release)
+if ban['ip'] ~= networkid then
+print("This user is connecting with a new IP.")
+else
+print("This user is connecting with the same credidentials.")
+end
+]]
